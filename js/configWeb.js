b69d678d4ccce2a797207519e4ecaaa10ef4695e
Esteban-cl <67084237+Esteban-cl@users.noreply.github.com>
2024-09-04 02:09:36 -0500

    feat: Nuevo elemento de musica

--- js/configWeb.js
+++ js/configWeb.js
@@ -9,7 +9,64 @@ paceOptions = {
     target: '#preloader .preloader-content'
 };
 
-$(document).ready(function() {
+function iniciarAudio() {
+    const audioContext = new window.AudioContext()
+    const iframeElement = document.getElementById('soundcloud-iframe');
+    const widget = SC.Widget(iframeElement)
+    
+    widget.bind(SC.Widget.Events.READY, function() {
+        widget.setVolume(5)
+        widget.seekTo(5000) 
+    })
+
+    if (audioContext.state === 'suspended') {
+        const options = {
+            closeButton: true,
+            debug: false,
+            newestOnTop: false,
+            progressBar: false,
+            positionClass: "toast-top-right",
+            preventDuplicates: false,
+            onclick: null,
+            showDuration: "300",
+            hideDuration: "1000",
+            timeOut: "0", // 5000 milisegundos
+            extendedTimeOut: "0", // 1000 milisegundos
+            showEasing: "swing",
+            hideEasing: "linear",
+            showMethod: "fadeIn",
+            hideMethod: "fadeOut",
+            "onclick": function () {
+                widget.play();
+                toastr.success('El sonido ha sido activado. Puedes desactivarlo el boton inferior izquierdo.', 'Sonido Activado');
+            }
+        };
+        toastr.info('Quieres activar el sonido? Da click en esta alerta.', 'Activar Sonido', options);
+    }
+}
+
+$(document).ready(function () {
     document.getElementById("ak_js_1").setAttribute("value", (new Date()).getTime());
     document.body.className = document.body.className.replace("siteorigin-panels-before-js", "");
+
+    iniciarAudio();
+
+    // Configuración de toastr.js
+    toastr.options = {
+        'closeButton': true,
+        'debug': false,
+        'newestOnTop': false,
+        'progressBar': false,
+        'positionClass': 'toast-top-right',
+        'preventDuplicates': false,
+        'showDuration': '1000',
+        'hideDuration': '1000',
+        'timeOut': '5000',
+        'extendedTimeOut': '1000',
+        'showEasing': 'swing',
+        'hideEasing': 'linear',
+        'showMethod': 'fadeIn',
+        'hideMethod': 'fadeOut',
+    }
+
 });
